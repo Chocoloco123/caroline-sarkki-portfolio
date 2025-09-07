08300b97d1ee3fa739fda3ee1618ce12210bf635
Chocoloco123 <csarkki.swe@gmail.com>
2025-09-07 15:59:34 -0700

    added vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 import './globals.scss'
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         <div className="app">
           {children}
         </div>
+        <SpeedInsights />
       </body>
     </html>
   )
